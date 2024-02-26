75d477af853bff35757085f090f65876b728fd60
Yarrabati Venkatesh Naidiu <yarrabati07@gmail.com>
2024-02-27 00:41:35 +0530

    added files

---
+++ src/com/java/Scannerdemo/Scannerdemo.java
@@ -0,0 +1,13 @@
+package com.java.Scannerdemo;
+
+import java.util.Scanner;
+
+public class Scannerdemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //
+        System.out.println(n);
+
+    }
+}
---
+++ src/com/java/threadsdemo/threadsdemo.java
@@ -0,0 +1,8 @@
+package com.java.threadsdemo;
+
+
+
+public class threadsdemo  extends Thread{
+    int i =10;
+
+}
